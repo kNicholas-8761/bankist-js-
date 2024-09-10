f474df6cd285550a5d14846984bece2b7758a50b
kbrown <kamauri205@gmail.com>
2024-09-10 17:12:59 +1000

    insert html in movements element using insertAdjacentHTML method

--- script.js
+++ script.js
@@ -63,31 +63,18 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 const displayMovements = function (movements) {
   movements.forEach(function (mov, i) {
-    const type = mov > 0 ? 'deposit' : 'withdrawl';
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
     <div class="movements__row">
        <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-       <div class="movements__value">${mov}€</div>
+       <div class="movements__value">${mov}</div>
      </div>
  `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
 
 displayMovements(account1.movements);
-
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
-
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-/////////////////////////////////////////////////
